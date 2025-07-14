59951f77482e775e3eaf371402d62db3beb279fb
ikram713 <i.messaoud@esi-sba.dz>
2025-07-14 16:37:25 +0200

    add profileRoutes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const searchRoutes = require('./routes/searchRoutes');
 const authRoutes = require('./routes/authRoutes');
 const likeRoutes = require('./routes/likeRoutes');
 const cartRoutes = require("./routes/cartRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 const cors = require('cors');
 require('dotenv').config();
 
@@ -31,6 +32,7 @@ app.use('/search', searchRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api', likeRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/profile", profileRoutes);
 
 app.listen(5000, '0.0.0.0', () => {
   console.log('Server running on http://0.0.0.0:5000');
